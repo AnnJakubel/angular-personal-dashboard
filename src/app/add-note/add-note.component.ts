22a55728f6da6406f71701e5af74e47c0f0d2c75
AnnJakubel <annabel.jakubel@gmail.com>
2023-06-06 22:05:11 +0300

    Form Validation

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -18,8 +18,8 @@ export class AddNoteComponent implements OnInit {
   }
 
   onFormSubmit(form: NgForm) {
+    if (form.invalid) return alert("Form is invalid");
     const note = new Note(form.value.title, form.value.content);
-    console.log(note);
 
     this.noteService.addNote(note);
     this.router.navigateByUrl("/notes");
